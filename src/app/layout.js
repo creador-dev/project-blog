1ec05364bc04e27b9595fe10c1f1e1d58602f185
Pawan Kumar <pwnkumar201@gmail.com>
2025-01-30 23:23:02 +0530

    Dark mode addded.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,60 +1,53 @@
-import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
-import clsx from 'clsx';
+import React from "react";
+import { Work_Sans, Spline_Sans_Mono } from "next/font/google";
+import clsx from "clsx";
+import { cookies } from "next/headers";
 
 import {
   BLOG_TITLE,
+  COLOR_THEME_COOKIE_NAME,
   LIGHT_TOKENS,
   DARK_TOKENS,
-} from '@/constants';
+} from "@/constants";
 
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import RespectMotionPreferences from '@/components/RespectMotionPreferences';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import RespectMotionPreferences from "@/components/RespectMotionPreferences";
 
-import './styles.css';
+import "./styles.css";
 
 const mainFont = Work_Sans({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family',
+  subsets: ["latin"],
+  display: "fallback",
+  weight: "variable",
+  variable: "--font-family",
 });
 const monoFont = Spline_Sans_Mono({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family-mono',
+  subsets: ["latin"],
+  display: "fallback",
+  weight: "variable",
+  variable: "--font-family-mono",
 });
 
 export const metadata = {
   title: BLOG_TITLE,
-  description: 'A wonderful blog about JavaScript',
+  description: "A wonderful blog about JavaScript",
 };
 
-function RootLayout({ children }) {
-  const theme = 'light';
+async function RootLayout({ children }) {
+  const savedTheme = (await cookies()).get(COLOR_THEME_COOKIE_NAME);
+  const theme = savedTheme?.value || "light";
 
   return (
     <RespectMotionPreferences>
       <html
         lang="en"
-        className={clsx(
-          mainFont.variable,
-          monoFont.variable
-        )}
+        className={clsx(mainFont.variable, monoFont.variable)}
         data-color-theme={theme}
-        style={
-          theme === 'light'
-            ? LIGHT_TOKENS
-            : DARK_TOKENS
-        }
+        style={theme === "light" ? LIGHT_TOKENS : DARK_TOKENS}
       >
         <body>
-          <Header theme={theme} />
+          <Header initialTheme={theme} />
           <main>{children}</main>
           <Footer />
         </body>
